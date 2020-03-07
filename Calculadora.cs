76090a8a7c533ddeb808574a73d7ad132d5b5b23
Welbjr <61892499+Welbjr@users.noreply.github.com>
2020-03-06 21:25:31 -0300

    Add files via upload

---
+++ Calculadora.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Calculadora
+    {
+        private List<double> listNumero = new List<double>();
+
+        public void addNumero(double num)
+        {
+            this.listNumero.Add(num);
+
+        }
+
+        public double somar()
+        {
+            double resultado = 0;
+            foreach (double numero in this.listNumero)
+            {
+                resultado += numero;
+            }
+            this.listNumero.Clear();
+            return resultado;
+        }
+
+        public double subtrair()
+        {
+            double resultado = this.listNumero[0];
+            this.listNumero.RemoveAt(0);
+            foreach (double numero in this.listNumero)
+            {
+                resultado -= numero;
+            }
+            this.listNumero.Clear();
+            return resultado;
+        }
+
+        public double multiplicador()
+        {
+            double resultado = this.listNumero[0];
+            this.listNumero.RemoveAt(0);
+            foreach (double numero in this.listNumero)
+            {
+                resultado *= numero;
+            }
+            this.listNumero.Clear();
+            return resultado;
+        }
+
+        public double dividir()
+        {
+            double resultado = this.listNumero[0];
+            this.listNumero.RemoveAt(0);
+            foreach (double numero in this.listNumero)
+            {
+                resultado /= numero;
+            }
+            this.listNumero.Clear();
+            return resultado;
+        }
+    }
+}
